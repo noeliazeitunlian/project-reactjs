845489758de50067d8c23e5c4c192575c4002f25
Noelia Zeitunlian <noeliazeitunlian@gmail.com>
2022-12-03 19:01:18 -0300

    agrego products

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -15,7 +15,7 @@ const Item = ({info}) => {
                     <Link to={`/detail/${info.id}`}>
                     <img src={info.image} className="cart-item-image" alt=""/>
                     <p className="cart-item-title">{info.title}</p>
-                    <p className="cart-item-title">${info.price}ARS</p>
+                    <p className="cart-item-title">${info.price} USD</p>
                     </Link>
 
                 </div>
